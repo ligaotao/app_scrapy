30da2b63a6e1d278b0a320a1bc880e6ce0c0d6f2
lee <774206981@qq.com>
2020-07-24 17:29:56 +0800

    1.完成从数据中更新比赛详情 2.停止规则有问题 看怎么优化一下逻辑

--- wegame/spiders/battle_detail.py
+++ wegame/spiders/battle_detail.py
@@ -12,14 +12,32 @@ class BattleDetailSpider(scrapy.Spider):
     # start_urls = ['https://m.wegame.com.cn/api/mobile/lua/proxy/index/mwg_tft_proxy//get_total_tier_rank_list']
 
     def __init__(self, *args, **kwargs):
+        self.area_user_cache = [{} for i in range(0, 100)]
         self.session = Sesssion()
 
     def start_requests(self):
-        self.battle = self.session.query(Battle).filter_by().all()
+        self.battle = self.session.query(Battle).filter(Battle.state ==None).all()
         self.index = 0
         self.battle_len = len(self.battle)
         # yield self.request(290)
         yield self.request(0)
+
+    def get_user_cache(self, area_id, slol_id):
+        cache = self.area_user_cache
+        if cache[area_id] is not None and cache[area_id].get('slol_id', None) is not None:
+            return cache[area_id].get('slol_id', None)
+        else:
+            return None
+    
+    def set_user_cache(self, area_id, slol_id, obj):
+        cache = self.area_user_cache
+        if cache[area_id] is None:
+            cache[area_id] = {}
+        user_obj = {}
+        user_obj[slol_id] = obj
+        cache[area_id].update(user_obj)
+        return obj
+        
 
     def request(self, index):
         
@@ -27,7 +45,7 @@ class BattleDetailSpider(scrapy.Spider):
             obj = self.battle[index]
             area_id = obj.area_id
             battle_id =  obj.id
-            slol_id = 'b7y00g4'
+            slol_id = obj.slol_id
 
             return get_battle_detail(
                 callback=self.parse,
@@ -48,14 +66,21 @@ class BattleDetailSpider(scrapy.Spider):
         obj = result['data']['battle_detail']
         for player in obj.get('player_list', []):
             rank_info = player.get('rank_info')
+            slol_id = player.get('slol_id')
+            
             try:
-                user_instance, created = select_or_insert(session, User, slol_id=player.get('slol_id'), area_id=body.get('area_id'), defaults={
-                    'name': player.get('name'),
-                    'icon_id': player.get('icon_id'),
-                    'tier': rank_info.get('tier'),
-                    'rank': rank_info.get('queue')
-                })
-                user_id = user_instance.id
+                if self.get_user_cache(area_id, slol_id) is None:
+                    user_instance, created = select_or_insert(session, User, slol_id=slol_id, area_id=area_id, defaults={
+                        'name': player.get('name'),
+                        'icon_id': player.get('icon_id'),
+                        'tier': rank_info.get('tier'),
+                        'rank': rank_info.get('queue')
+                    })
+                    self.set_user_cache(area_id, slol_id, {'id': user_instance.id})
+                    user_id = user_instance.id
+                else:
+                    user_id = self.get_user_cache(area_id, slol_id).get('id')
+                
                 battle_id = obj.get('battle_id')
                 battle_schema = {
                     'gold_left': player.get('gold_left', ''),
@@ -70,7 +95,7 @@ class BattleDetailSpider(scrapy.Spider):
                     'total_damage_to_players': player.get('total_damage_to_players', ''),
                     'total_trait_num': player.get('total_trait_num', ''),
                     'duration': obj.get('duration', ''),
-                    'battle_ranking': obj.get('battle_ranking', 0),
+                    'battle_ranking': player.get('battle_ranking', 0),
                 }
                 battle_detail_instance, created = update_or_create(session, BattleDetail, battle_id=battle_id, user_id=user_id,defaults=battle_schema)
                 # 首先删除本场比赛的所有相关信息
--- wegame/spiders/battle_list.py
+++ wegame/spiders/battle_list.py
@@ -2,7 +2,7 @@ import scrapy
 import json
 import logging
 import datetime
-from wegame.data.model import Sesssion, Battle, update_or_create
+from wegame.data.model import Sesssion, Battle, BattleDetail, User,update_or_create
 from wegame.api import get_battle_list
 
 class BattleListSpider(scrapy.Spider):
@@ -10,10 +10,16 @@ class BattleListSpider(scrapy.Spider):
     allowed_domains = ['wegame.com.cn']
     # start_urls = ['https://m.wegame.com.cn/api/mobile/lua/proxy/index/mwg_tft_proxy//get_total_tier_rank_list']
 
+    def __init__(self):
+        self.session = Sesssion()
+
     def start_requests(self):
-        yield self.request(290)
+        last_battle = self.session.query(Battle).join(BattleDetail).join(User).filter(User.slol_id=='b7y00g4', User.area_id==9).order_by(Battle.start_time.desc()).first()
+        self.last_battle = last_battle
+        yield self.request(0)
 
     def request(self, offset):
+        
         return get_battle_list(
             callback=self.parse,
             body=json.dumps({
@@ -30,13 +36,18 @@ class BattleListSpider(scrapy.Spider):
         body = json.loads(response.request.body)
         area_id = body.get('area_id')
         session = Sesssion()
+        start_time = None
         try:
             for obj in result['data']['battle_list']:
+                start_time = datetime.datetime.fromtimestamp(obj.get('start_time'))
                 battle_schema = {
                     'game_mode': obj.get('game_mode'),
-                    'start_time': datetime.datetime.fromtimestamp(obj.get('start_time')),
+                    'start_time': start_time,
                     'area_id': area_id
                 }
+                if self.last_battle and self.last_battle.start_time >= start_time:
+                    # 已经更新到历史数据 触发终止条件
+                    result['data']['next_offset'] = -1
                 update_or_create(session, Battle, id=obj.get('battle_id'),defaults=battle_schema)
         except Exception as e:
             print(e)
